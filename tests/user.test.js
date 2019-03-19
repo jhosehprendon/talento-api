c0a9fa6a4ae37920eb52ebb6ece57ccfebf45cf0
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-03-19 13:37:13 -0500

    Added login test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,30 @@
-const request = require('supertest')
-const app = require('../app')
+const request = require('supertest');
+const mongoose = require('mongoose')
+const app = require('../app');
+const User = require('../api/models/user');
 
-test('should signup a new user', async () => {
+const userOne = {
+    name: 'Homer Simpson',
+    email: 'homer@gmail.com',
+    password: 'homer!!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await request(app).post('/user/signup').send(userOne)
+})
+
+test('Should signup a new user', async () => {
     await request(app).post('/user/signup').send({
         name: 'Chino',
         email: 'chino@gmail.com',
         password: 'chino123'
     }).expect(201)
+})
+
+test('Should login an existing user', async () => {
+    await request(app).post('/user/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
 })
